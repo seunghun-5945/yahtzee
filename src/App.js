f3a4a8150945757eaddb0169aa04d109a0d77957
Yeom1009 <yjg5641@oasis.inje.ac.kr>
2024-11-10 00:45:18 +0900

    register added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-function App() {
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: salmon;
+`;
+
+const Frame = styled.div`
+  width: 25%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 40px;
+  border-radius: 10px;
+`;
+
+const Button = styled.button`
+  width: 300px;
+  height: 40px;
+  border-radius: 10px;
+`
+
+
+const App = () =>{
+
+const[id,setID] = useState("");
+const[pw,setPW] = useState("");
+const[IdError,setIdError] = useState("");
+const[PwError,setPwError] = useState("");
+
+const testID = 1234
+const testPW = 1234
+
+const backendLogin = async () => {
+  try {
+    const response = await axios.post("https://port-0-yhatzeeback-m39re8g35ae5a423.sel4.cloudtype.app/login", {
+      "name": id,
+      "password": pw,
+    })
+    console.log(response);
+    setIdError('');
+    setPwError('');
+    alert("로그인 성공");
+  }
+  catch(error) {
+    console.log("에러남:", error);
+    alert("로그인 실패");
+  }
+}
+
+
+
+const handleChangeId = (e) => {
+  setID(e.target.value);
+  console.log(id);
+}
+const handleChangePW = (e) => {
+  setPW(e.target.value);
+  console.log(pw);
+}
+
+  useEffect(()=>{
+    if(id.length < 10){
+      setIdError("아이디가 10자 이하입니다.");
+    }else{
+      setIdError('');
+    }
+
+  },[id]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Container>
+    <Frame>
+    <h1>로그인하세요</h1>
+    <br/><br/>
+  <Input
+    type="text"
+    placeholder=" E-mail"
+    onChange={handleChangeId}
+  />
+  <div style={{
+    width: "80%",
+    height: "20px", 
+    color: "red" 
+  }}>{IdError}</div>
+  <br/>
+  <Input
+    type="password"
+    placeholder=" 비밀번호"
+    onChange={handleChangePW}
+  />
+  <div style={{
+    width: "80%",
+    height: "20px",
+    color: "red" 
+  }}>{PwError}</div>
+  <br/><br/>
+  <div></div>
+
+  <Link to="/Register">
+    <span>회원가입</span>
+  </Link>
+
+  <Button
+    onClick={backendLogin}
+    >로그인</Button>
+    </Frame>
+  </Container>
+  )
 }
 
 export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: salmon;
+  background: linear-gradient(to right, green, blue);
+`;
+
+const Frame = styled.div`
+  width: 25%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+const Input = styled.input`
+  width: 300px;
+  height: 40px;
+  border-radius: 10px;
+`
+
+const Button = styled.button`
+  width: 300px;
+  height: 40px;
+  border-radius: 10px;
+`
+
+const Register = () => {
+
+  const[id,setID] = useState("");
+  const[pw,setPW] = useState(""); 
+  const[IdError,setIdError] = useState("");
+  const[PwError, setPwError] = useState("");
+
+  const backendRegister = async () => {
+    // 공백 및 에러가 있는 경우 회원가입 막기
+    if (!id || !pw || IdError || PwError) {
+      alert("아이디와 비밀번호를 올바르게 입력해주세요.");
+      return;
+    }
+    try {
+      const response = await axios.post("https://port-0-yhatzeeback-m39re8g35ae5a423.sel4.cloudtype.app/register", {
+        "name": id,
+        "password": pw,
+      })
+      console.log(response);
+      alert("회원가입 성공");
+    }
+    catch(error) {
+      console.log("에러남:", error);
+      alert("회원가입 실패");
+    }
+  }
+
+  useEffect(() => {
+    const idTest = async () => {
+      try {
+        const response = await axios.post("https://port-0-yhatzeeback-m39re8g35ae5a423.sel4.cloudtype.app/alreadyusingname", {
+          "name": id
+        })
+        console.log(response.data);
+        
+        if(response.data == true) {
+          console.log("아이디가 중복됩니다");
+          setIdError('아이디가 중복됩니다.');
+        }
+        else {
+          console.log();
+        }
+      }
+      catch(error) {
+        console.log("에러남:", error);
+        alert("로그인 실패");
+      }
+    }
+    idTest();  
+  }, [id])
+
+  const handleChangeId = (e) => {
+    const newId = e.target.value;
+    setID(newId);
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!newId) {
+      setIdError("아이디를 입력하세요.");
+    } else if (!emailPattern.test(newId)) {
+      setIdError("유효한 이메일 주소를 입력하세요.");
+    } else {
+      setIdError("");
+    }
+  };
+
+  const handleChangePW = (e) => {
+    const newPw = e.target.value;
+    setPW(newPw);
+
+    if (!newPw) {
+      setPwError("비밀번호를 입력하세요.");
+    } else if (/\s/.test(newPw)) {
+      setPwError("비밀번호에 공백이 포함될 수 없습니다.");
+    } else {
+      setPwError("");
+    }
+  };
+
+  return (
+  <Container>
+   <Frame>
+  <h4 style={{
+    width: "80%",
+    height: "30px" 
+  }}>아이디 <span style={{color:'red'}}>*</span></h4>
+  <Input
+  type="text"
+  placeholder=" E-mail"
+  onChange={handleChangeId}
+  /> 
+  <div style={{
+    width: "80%",
+    height: "20px",
+    color: "red"
+  }}>{IdError}</div>
+  <br/>
+  <h4 style={{
+    width: "80%",
+    height: "30px" 
+  }}>비밀번호 <span style={{color:'red'}}>*</span></h4>
+  <Input
+  placeholder=" 비밀번호"
+  onChange={handleChangePW}
+  />
+  <br/><br/><br/>
+  <Button
+  onClick={backendRegister}
+  >회원가입</Button>
+   </Frame>
+  </Container>
+  );
+};
+
+export default Register;
